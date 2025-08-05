37b3e1658aa3f4d04a3680af4e713bdfef21289d
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-08-05 11:52:53 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+export enum ViewMode {
+  First = 'first',
+  Second = 'second',
+  Third = 'third'}
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  viewMode = ViewMode;
+  currentViewMode: ViewMode = ViewMode.First;
 }
